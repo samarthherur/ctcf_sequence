6cc7350d674cfe8b00c96d575e6e878316249577
samarthherur <samarth.herur@students.iiserpune.ac.in>
2025-07-01 19:01:20 +0530

    commit

---
+++ fimo_analysis/fimo.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#SBATCH --job-name=fimo
+#SBATCH --output=fimo.out
+#SBATCH --error=fimo.err
+#SBATCH --time=10:00:00
+#SBATCH --partition=gpu
+#SBATCH --ntasks=40
+#SBATCH --nodelist=cn12
+
+python /home/samarth/ctcf_sequence/fimo_analysis/fimo_neighbourhood_analysis.py \
+  --fimo_filepath   /home/samarth/ctcf_sequence_data/input_data/h1_human_data/fimo.tsv \
+  --genome_fasta    /home/samarth/ctcf_sequence_data/input_data/genome_files/hg38.fa \
+  --chip_filepath   /home/samarth/ctcf_sequence_data/input_data/h1_human_data/chip_h1_hg38_narrowpeak.bed \
+  --boundary        /home/samarth/ctcf_sequence_data/input_data/h1_human_data/4DNFIED5HLDC_sorted.bed \
+  --output_dir      /home/samarth/ctcf_sequence_data/output/fimo/H1esc_CTCF_50_50 \
+  --n_a 50 \
+  --n_b 50 \
+  --ignore_repeats
+    
+python /home/samarth/ctcf_sequence/fimo_analysis/fimo_neighbourhood_analysis.py \
+  --fimo_filepath   /home/samarth/ctcf_sequence_data/input_data/h1_human_data/fimo.tsv \
+  --genome_fasta    /home/samarth/ctcf_sequence_data/input_data/genome_files/hg38.fa \
+  --chip_filepath   /home/samarth/ctcf_sequence_data/input_data/h1_human_data/chip_h1_hg38_narrowpeak.bed \
+  --boundary        /home/samarth/ctcf_sequence_data/input_data/h1_human_data/4DNFIED5HLDC_sorted.bed \
+  --output_dir      /home/samarth/ctcf_sequence_data/output/fimo/HFF_CTCF_50_50 \
+  --n_a 50 \
+  --n_b 50 \
+  --ignore_repeats
+
+python /home/samarth/ctcf_sequence/fimo_analysis/fimo_neighbourhood_analysis.py \
+  --fimo_filepath   /home/samarth/ctcf_sequence_data/input_data/h1_human_data/fimo.tsv \
+  --genome_fasta    /home/samarth/ctcf_sequence_data/input_data/genome_files/hg38.fa \
+  --chip_filepath   /home/samarth/ctcf_sequence_data/input_data/h1_human_data/chip_h1_hg38_narrowpeak.bed \
+  --boundary        /home/samarth/ctcf_sequence_data/input_data/h1_human_data/4DNFIED5HLDC_sorted.bed \
+  --output_dir      /home/samarth/ctcf_sequence_data/output/fimo/H1esc_CTCF_0_0 \
+  --n_a 0 \
+  --n_b 0 \
+  --ignore_repeats
+
+python /home/samarth/ctcf_sequence/fimo_analysis/fimo_neighbourhood_analysis.py \
+  --fimo_filepath   /home/samarth/ctcf_sequence_data/input_data/h1_human_data/fimo.tsv \
+  --genome_fasta    /home/samarth/ctcf_sequence_data/input_data/genome_files/hg38.fa \
+  --chip_filepath   /home/samarth/ctcf_sequence_data/input_data/h1_human_data/chip_h1_hg38_narrowpeak.bed \
+  --boundary        /home/samarth/ctcf_sequence_data/input_data/h1_human_data/4DNFIED5HLDC_sorted.bed \
+  --output_dir      /home/samarth/ctcf_sequence_data/output/fimo/HFF_CTCF_0_0 \
+  --n_a 0 \
+  --n_b 0 \
+  --ignore_repeats
+
+
+
+
+
+
